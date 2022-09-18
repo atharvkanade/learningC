287b2b35d5acb78dd0682e77dbe6e7bb8625f584
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-09-18 09:42:59 +0530

    learned variables

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main()
+{
+int a;
+printf("enter a :");
+scanf("%d",&a);
+
+int b;
+printf("enter b :");
+scanf("%d",&b);
+
+int sum;
+printf("sum of two number is : %d \n", a+b);
+scanf("%d",&sum);
+return 0;
+
+}
