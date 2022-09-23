4ebf405cc9abce57a9079800c21561f838c55c95
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-09-23 20:17:17 +0530

    Greatest of two numbers

---
+++ greater.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a ;
+    printf("Enter  a : ");
+     scanf("%d",&a);
+  int b ;
+     printf("Enter  b : ");
+    scanf("%d",&b);
+
+    if (a > b)
+    {
+      printf("a is greater ");
+    }
+    else
+    {
+      printf(" b is greater ");
+    }
+
+
+    return 0;
+}
