f1dd3349dc031b3e4b01e2a7fe86d29a4bf030ec
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-10-20 23:03:02 +0530

    Hollow Pyramid

---
+++ pyramid2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k, r;
+    printf("Enter number of rows : ");
+    scanf("%d", &r);
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int k = r; k > i + 1; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i * 2; j++)
+        {
+            if (i == r - 1)
+            {
+                printf("*");
+            }
+            else
+            {
+                if (j == 0 || j >= i * 2)
+                {
+                    printf("*");
+                }
+                else
+                {
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
