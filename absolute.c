1d546c6a925310da58f0a586fef077dd283aa45f
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-11-15 07:43:36 +0530

    Find absloute value of given integer

---
+++ absolute.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a number :");
+    scanf("%d", &n);
+
+    printf("The absolute value of given number is :%d", abs(n));
+
+    return 0;
+}
