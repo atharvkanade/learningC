f7807b15ff5bec43466da542e80d13459b477b4f
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-11-18 15:53:01 +0530

    To print pascal triangle

---
+++ pascal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c = 1, k, i, j;
+    printf("Input number of rows: ");
+    scanf("%d", &r);
+    for (i = 0; i < r; i++)
+    {
+        for (k = 1; k <= r - i; k++)
+            printf("  ");
+        for (j = 0; j <= i; j++)
+        {
+            if (j == 0 || i == 0)
+                c = 1;
+            else
+                c = c * (i - j + 1) / j;
+            printf("% 4d", c);
+        }
+        printf("\n");
+    }
+}
