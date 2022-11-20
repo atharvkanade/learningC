2edbf8b2233d2038aed62a2027f47f469a59ecac
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-11-20 20:44:27 +0530

    Print the Floyd's Triangle

---
+++ Floyd's.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, p, q, r;
+    printf("Enter number of rows :");
+    scanf("%d", &r);
+
+    for (i = 1; i <= r; i++)
+    {
+        if (i % 2 == 0)
+        {
+            p = 1;
+            q = 0;
+        }
+        else
+        {
+            p = 0;
+            q = 1;
+        }
+        for (j = 1; j <= i; j++)
+        {
+            if (j % 2 == 0)
+            {
+                printf("%d", p);
+            }
+            else
+            {
+                printf("%d", q);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
