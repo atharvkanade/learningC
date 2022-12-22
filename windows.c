dcb1c80c28255e0b09904b07f8c8275a3577f1d6
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-12-22 22:14:33 +0530

    Program to shutdown Windows

---
+++ windows.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char ch;
+
+    printf("Do you want to shutdown the PC- (y/n) ?\n");
+    scanf("%c", &ch);
+
+    if (ch == 'y' || ch == 'Y')
+    {
+        system("C:\\WINDOWS\\System32\\shutdown -s");
+    }
+
+    return 0;
+}
