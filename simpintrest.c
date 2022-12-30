4daf9f66ecea2b3f54a4bed47016e9836befa83d
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2022-12-30 19:22:13 +0530

    To find simple intrest

--- simpintrest.c
+++ simpintrest.c
@@ -2,21 +2,16 @@
 
 void main()
 {
-    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
     float principal_amt, rate, simple_interest;
     int time;
-    printf("Enter the value of principal amount, rate and time\n\n\n");
+    printf("Enter the value of principal amount, rate and time \n");
     scanf("%f%f%d", &principal_amt, &rate, &time);
 
-    // considering rate is in percentage
     simple_interest = (principal_amt * rate * time) / 100.0;
 
-    // usually used to align text in form of columns in table
-    printf("\n\n\t\t\tAmount = Rs.%7.3f\n ", principal_amt);
+    printf("Amount = Rs.%7.3f\n ", principal_amt);
 
-    printf("\n\n\t\t\tRate = Rs.%7.3f\n ", rate);
-    printf("\n\n\t\t\tTime= %d years \n", time);
-    printf("\n\n\t\t\tSimple Interest = Rs.%7.3f\n ", simple_interest);
-    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
-    return 0;
+    printf("Rate = Rs.%7.3f\n ", rate);
+    printf("Time= %d years \n", time);
+    printf("Simple Interest = Rs.%7.3f\n ", simple_interest);
 }
