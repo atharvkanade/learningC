dcab95174042694d456b489e8a4375c125c5c831
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-01-20 22:04:46 +0530

    Number Star Right Diamond Pattern Type4

---
+++ star.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, r;
+    printf("Enter the number of rows(odd) :\n");
+    scanf("%d", &r);
+    for (i = 0; i < r; i++)
+    {
+        if (i <= (r / 2))
+        {
+            for (j = 0; j <= i; j++)
+            {
+                if (j != 0)
+                {
+                    printf("*%d", i + 2);
+                }
+                else
+                {
+                    printf("%d", i + 2);
+                }
+            }
+        }
+        else
+        {
+            for (j = i; j < r; j++)
+            {
+                if (j != i)
+                {
+                    printf("*%d", r - i + 1);
+                }
+                else
+                {
+                    printf("%d", r - i + 1);
+                }
+            }
+        }
+
+        printf("\n");
+    }
+}
