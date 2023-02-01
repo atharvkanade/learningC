245ed40a52ceefc736bc7f707109e41e5acdb25f
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-02-01 23:51:00 +0530

    Swap to numbers using pointer

---
+++ ptr_swap.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b;
+    int *ptra, *ptrb;
+    int temp;
+
+    printf("Enter value for a: ");
+    scanf("%d", &a);
+
+    printf("Enter value for b: ");
+    scanf("%d", &b);
+
+    printf("The values before swapping are: a = %d     b = %d", a, b);
+
+    ptra = &a;
+    ptrb = &b;
+
+    temp = *ptra;
+    *ptra = *ptrb;
+    *ptrb = temp;
+
+    printf(" \n The values after swapping are: a = %d    b = %d", a, b);
+
+    return 0;
+}
